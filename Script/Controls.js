67c2c2158664d7c62c958f997905ccfdc8339942
dylululu <duynhatluu2003@gmail.com>
2024-06-08 11:04:14 +0700

    m

--- Script/Controls.js
+++ Script/Controls.js
@@ -7,9 +7,11 @@ audioStartsAt("BaoHong",11);
 audioStartsAt("water",1.5);
 audioStartsAt("magic",45);
 pauseAfterSeconds("may", 3.2);
-audioStartsAt("MinhA",196);
-pauseAfterSeconds("MinhA", 3, 0.1,0.2);
+audioStartsAt("MinhA",198);
+pauseAfterSeconds("MinhA", 2, 0.1,0.2);
 tangDan("Ket", 0);
+audioStartsAt("KXR",20);
+
 
 
 
